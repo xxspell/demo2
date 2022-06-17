60c74c3c153a99faeb6669626987d62d7dfee387
Eugene <eugenereuh@gmail.com>
2022-06-17 11:24:35 +0500

    add crud category

---
+++ modules/admin/models/Category.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\modules\admin\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "category".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property string $image
+ *
+ * @property Product[] $products
+ */
+class Category extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'category';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'description', 'image'], 'required'],
+            [['name', 'description', 'image'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'description' => 'Description',
+            'image' => 'Image',
+        ];
+    }
+
+    /**
+     * Gets query for [[Products]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProducts()
+    {
+        return $this->hasMany(Product::className(), ['category_id' => 'id']);
+    }
+}
---
+++ modules/admin/views/category/index.php
@@ -0,0 +1,48 @@
+<?php
+
+use app\modules\admin\models\Category;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $searchModel app\modules\admin\models\CategorySearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Categories';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="category-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Category', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php Pjax::begin(); ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            'description',
+            'image',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Category $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+    <?php Pjax::end(); ?>
+
+</div>
